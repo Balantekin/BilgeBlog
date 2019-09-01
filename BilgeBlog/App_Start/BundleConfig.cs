f067ede92ca77efa2152e8ae0791539d8afd3bfc
Buğra Balantekin <bugrabalantekin@hotmail.com>
2019-09-01 21:48:52 +0300

    javascriptler için bundle config eklendi

---
+++ BilgeBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BilgeBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/admin/layout").Include(
+                "~/Scripts/jquery.min.js",
+                "~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/jquery.easing.min.js",
+                "~/Scripts/sb-admin-2.min.js"
+
+                ));
+
+            bundles.Add(new cs)
+        }
+    }
+}
--- BilgeBlog/Global.asax.cs
+++ BilgeBlog/Global.asax.cs
@@ -1,8 +1,10 @@
+using BilgeBlog.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace BilgeBlog
@@ -11,6 +13,7 @@ namespace BilgeBlog
     {
         protected void Application_Start()
         {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
